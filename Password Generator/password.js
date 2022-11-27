fa7aabb584a655b2f95e676ebe03c55753ef12d1
Bernard Sapida <61785349+BernardSapida@users.noreply.github.com>
2022-11-27 08:54:00 +0800

    Update password.js

--- Password Generator/password.js
+++ Password Generator/password.js
@@ -3,29 +3,22 @@ function generatePassword(){
 	let lowercase = "abcdefghijklmnopqrstuvwxyz";
 	let numbers = '1234567890';
 	let symbols= "!@#$%&";
-
-
 	let capital = document.getElementById("uppercaseEl").checked
 	let notCapital = document.getElementById('lowercaseEl').checked
 	let numerals = document.getElementById('numbersEl').checked
 	let symbolVar = document.getElementById('symbolsEl').checked
 	let passLength = document.getElementById('customRange1').value
 	let charNum = document.getElementById('charNum')
-
-
 	let num="";
+	
 	if (capital == false && notCapital == false && numerals == false && symbolVar == false) console.log('click buttons down there'); 
 	if (notCapital == true) num+=lowercase;
 	if(capital == true)  num+=uppercase;
 	if (numerals == true) num+=numbers;
 	if (symbolVar == true)  num+=symbols;
 
-
 	let passChar = "";
-	for(i=0;i<passLength;i++){
-		passChar+=num.charAt(Math.floor(Math.random(num.length)*num.length));
-
-	}
+	for(i=0;i<passLength;i++) passChar+=num.charAt(Math.floor(Math.random(num.length)*num.length));
 	
 	document.getElementById("password").innerText= passChar;
 	document.getElementById('charNum').innerText="Number of Characters: "+ passLength;
